22fbca7dab6d282355f34ae77cd985c5ce6b782d
Hung Duong Quang <qhungg289@protonmail.com>
2022-06-17 21:42:46 +0700

    Update custom Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html lang="vi">
+		<Html lang="en">
 			<Head />
-			<body className="bg-zinc-800 text-zinc-200">
+			<body className="bg-zinc-900 text-zinc-200">
 				<Main />
 				<NextScript />
 			</body>
